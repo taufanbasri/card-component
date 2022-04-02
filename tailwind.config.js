230ea971675e9042c6abdeddd5473dbf06b5eeee
taufanbasri <taufanprasetyobasri@gmail.com>
2022-04-02 07:55:13 +0700

    Setup Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lexend': ['Lexend Deca', 'sans-serif'],
+        'shoulders': ['Big Shoulders Display', 'cursive']
+      },
+      colors: {
+        'bright-orange': 'hsl(31, 77%, 52%)',
+        'dark-cyan': 'hsl(184, 100%, 22%)',
+        'very-dark-cyan': 'hsl(179, 100%, 13%)',
+        'transparent-white': 'hsla(0, 0%, 100%, 0.75)',
+        'very-light-gray': 'hsl(0, 0%, 95%)'
+      }
+    },
   },
   plugins: [],
 }
